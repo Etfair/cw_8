01b42d70b37db75afac3c6ab77eedc10d7f8d4e7
Etfair <and90dmb@Mail.ru>
2023-10-21 11:25:22 +0300

    fix

--- config/settings.py
+++ config/settings.py
@@ -130,7 +130,6 @@ DATABASES = {
         # 'USER': os.getenv('DATABASES_DEFAULT_USER'),
         # 'PASSWORD': os.getenv('DATABASES_DEFAULT_PASSWORD'),
         # 'HOST': os.getenv('DB_HOST'),
-        'HOST': '127.0.0.1'
     }
 }
 
