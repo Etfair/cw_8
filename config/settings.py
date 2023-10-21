e63369e4939eab93de945833f7a2c00951fbef2b
Etfair <and90dmb@Mail.ru>
2023-10-21 11:23:53 +0300

    fix

--- config/settings.py
+++ config/settings.py
@@ -130,6 +130,8 @@ DATABASES = {
         # 'USER': os.getenv('DATABASES_DEFAULT_USER'),
         # 'PASSWORD': os.getenv('DATABASES_DEFAULT_PASSWORD'),
         # 'HOST': os.getenv('DB_HOST'),
+        'HOST': '127.0.0.1',
+        'PORT': 5432
     }
 }
 
